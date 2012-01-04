8a0a1dc1d5f2a7d397c55406d081799823031326
Marshall Huss <mhuss@mitre.org>
2012-01-04 10:58:09 -0500

    Added simile timeline

--- HReader/HRTimelineViewController.m
+++ HReader/HRTimelineViewController.m
@@ -48,10 +48,21 @@
     
     self.headerView.backgroundColor = [UIColor clearColor];
     
-    NSURL *html = [[NSBundle mainBundle] URLForResource:@"timeline-index" withExtension:@"html"];
-    NSURLRequest *request = [NSURLRequest requestWithURL:html];
+//    NSURL *html = [[NSBundle mainBundle] URLForResource:@"timeline-index" withExtension:@"html"];
+//    NSURLRequest *request = [NSURLRequest requestWithURL:html];
+//    [self.webView loadRequest:request];
+
+    
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"hReader_timeline" 
+                                                     ofType:@"html"
+                                                inDirectory:@"timeline"];
+    NSLog(@"Path: %@", path);
+    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL fileURLWithPath:path]];
     [self.webView loadRequest:request];
     
+//    [[NSNotificationCenter defaultCenter] addObserver:self
+//                                             selector:@selector(keyboardWillShow:)
+//                                                 name:UIKeyboardWillShowNotification object:nil];
 }
 
 - (void)viewDidUnload {
@@ -62,8 +73,24 @@
     [super viewDidUnload];
 }
 
+/*
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
 	return UIInterfaceOrientationIsLandscape(interfaceOrientation);
 }
+
+- (void)keyboardWillShow:(NSNotification *)aNotification {
+    [self performSelector:@selector(esconde) withObject:nil afterDelay:0];
+}
+
+- (void)esconde {
+    for (UIWindow *keyboardWindow in [[UIApplication sharedApplication] windows]) {
+        for (UIView *keyboard in [keyboardWindow subviews]) {
+            if([[keyboard description] hasPrefix:@"<UIPeripheralHostView"] == YES) {
+                [keyboard removeFromSuperview];   
+            }
+        }
+    }
+}
+ */
 
 @end
