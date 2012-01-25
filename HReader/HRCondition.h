f0a7691b16a456732925d02095da54b883214fc8
Marshall Huss <mhuss@mitre.org>
2012-01-25 14:37:10 -0500

    Added objects for condition and medication

---
+++ HReader/HRCondition.h
@@ -0,0 +1,13 @@
+//
+//  HRCondition.h
+//  HReader
+//
+//  Created by Marshall Huss on 1/25/12.
+//  Copyright (c) 2012 MITRE Corporation. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HRCondition : NSObject
+
+@end
---
+++ HReader/HRCondition.m
@@ -0,0 +1,13 @@
+//
+//  HRCondition.m
+//  HReader
+//
+//  Created by Marshall Huss on 1/25/12.
+//  Copyright (c) 2012 MITRE Corporation. All rights reserved.
+//
+
+#import "HRCondition.h"
+
+@implementation HRCondition
+
+@end
