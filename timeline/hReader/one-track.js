388cfa9f84785ad43c2159ef3f32f007559d8f5d
Marshall Huss <mhuss@mitre.org>
2012-03-28 16:08:37 -0400

    New timeline files

---
+++ timeline/hReader/one-track.js
@@ -0,0 +1,6 @@
+Timeline.OriginalEventPainter.prototype._findFreeTrack = function(event, rightEdge) {
+    var trackAttribute = event.getTrackNum();
+        return 0; 
+         //All the events are on one track
+
+};
