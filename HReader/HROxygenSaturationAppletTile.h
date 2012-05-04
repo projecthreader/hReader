d99b386e67f346d7fb4344d19069e1cd37c98dfb
Caleb Davenport <cdavenport@mitre.org>
2012-05-04 16:43:05 -0400

    add new vital applet tile superclass to help with wire framing new vital applets

---
+++ HReader/HROxygenSaturationAppletTile.h
@@ -0,0 +1,13 @@
+//
+//  HROxygenSaturationAppletTile.h
+//  HReader
+//
+//  Created by Caleb Davenport on 5/4/12.
+//  Copyright (c) 2012 MITRE Corporation. All rights reserved.
+//
+
+#import "HRAppletTile.h"
+
+@interface HROxygenSaturationAppletTile : HRAppletTile
+
+@end
