07aa1d9fef50a9ee7f5bf47fe17de9c09d5ae640
Caleb Davenport <cdavenport@mitre.org>
2012-05-11 16:00:51 -0400

    show n more in medications applet

--- HReader/HRMedicationsAppletTile.m
+++ HReader/HRMedicationsAppletTile.m
@@ -20,29 +20,34 @@
 - (void)tileDidLoad {
     [super tileDidLoad];
     NSArray *medications = [[self patient] medications];
+    NSArray *nameLabels = [self.medicationLabels sortedArrayUsingKey:@"tag" ascending:YES];
+    NSArray *dosageLabels = [self.dosageLabels sortedArrayUsingKey:@"tag" ascending:YES];
     NSUInteger medicationsCount = [medications count];
-    [[self.medicationLabels sortedArrayUsingKey:@"tag" ascending:YES] enumerateObjectsUsingBlock:^(UILabel *label, NSUInteger index, BOOL *stop) {
-        if (index < medicationsCount) {
-            HRMEntry *entry = [medications objectAtIndex:index];
-            label.text = [entry.desc sentenceCapitalizedString];
+    NSUInteger labelCount = [nameLabels count];
+    BOOL showCountLabel = (medicationsCount > labelCount);
+    NSAssert(labelCount == [dosageLabels count], @"There must be an equal number of name and date labels");
+    [nameLabels enumerateObjectsUsingBlock:^(UILabel *label, NSUInteger index, BOOL *stop) {
+        
+        // this is the last label and we should show count
+        if (index == labelCount - 1 && showCountLabel) {
+            label.text = [NSString stringWithFormat:@"%lu more…", medicationsCount - labelCount + 1];
         }
+        
+        // normal condition label
+        else if (index < medicationsCount) {
+            HRMEntry *condition = [medications objectAtIndex:index];
+            label.text = condition.desc;
+        }
+        
+        // no conditions
         else if (index == 0) { label.text = @"None"; }
+        
+        // clear the label
         else { label.text = nil; }
+        
     }];
-    [[self.dosageLabels sortedArrayUsingKey:@"tag" ascending:YES] enumerateObjectsUsingBlock:^(UILabel *label, NSUInteger idx, BOOL *stop) {
-//        if (idx < medicationsCount) {
-//            HRMEntry *entry = [medications objectAtIndex:idx];
-//            NSDictionary *dose = entry.dose;
-//            if ([dose count] > 0) {
-//                label.text = [dose description];   
-//            }
-//            else {
-//                label.text = @"";
-//            }
-//        }
-//        else {
-            label.text = nil;
-//        }
+    [dosageLabels enumerateObjectsUsingBlock:^(UILabel *label, NSUInteger index, BOOL *stop) {
+        label.text = nil;
     }];
 }
 
