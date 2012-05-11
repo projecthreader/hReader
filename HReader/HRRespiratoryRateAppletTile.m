2ac0d394f647a038581b09abec88315309561d18
Administrator <administrator@mm188098-pc.mitre.org>
2012-05-11 10:49:32 -0400

    minor corrections to respiratory rate applet

--- HReader/HRRespiratoryRateAppletTile.m
+++ HReader/HRRespiratoryRateAppletTile.m
@@ -33,19 +33,13 @@
     HRMEntry *latest = [__entries lastObject];
     
     // set labels
-    self.titleLabel.text = @"Respiratory Rate";
-    self.leftTitleLabel.text = @"RECENT RESULT:";
     NSInteger latestValue = [[latest valueForKeyPath:@"value.scalar"] integerValue];
     self.leftValueLabel.text = [NSString stringWithFormat:@"%lu", (long)latestValue];
-    self.leftValueLabel.adjustsFontSizeToFitWidth = YES;
     self.leftValueLabel.textColor = [self isValueNormal:latestValue] ? [UIColor blackColor] : [HRConfig redColor];
-    self.middleTitleLabel.text = @"DATE:";
     self.middleValueLabel.text = [latest.date shortStyleDate];
-    self.middleValueLabel.adjustsFontSizeToFitWidth = YES;
     self.rightValueLabel.text = [NSString stringWithFormat:@"%lu-%lu",
                                  [self normalLow],
                                  [self normalHigh]];
-    self.rightValueLabel.adjustsFontSizeToFitWidth = YES;
     
     // sparkline    
     HRSparkLineRange range = HRMakeRange([self normalLow], [self normalHigh] - [self normalLow]);
