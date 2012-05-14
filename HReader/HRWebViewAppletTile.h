aa77aa93bb2f242e0d4561a2caa853473ebd3e3b
Marshall Huss <mhuss@mitre.org>
2012-05-14 10:43:31 -0400

    Started webview applet

---
+++ HReader/HRWebViewAppletTile.h
@@ -0,0 +1,14 @@
+//
+//  HRWebViewAppletTile.h
+//  HReader
+//
+//  Created by Marshall Huss on 5/14/12.
+//  Copyright (c) 2012 MITRE Corporation. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "HRAppletTile.h"
+
+@interface HRWebViewAppletTile : HRAppletTile <UIWebViewDelegate>
+
+@end
