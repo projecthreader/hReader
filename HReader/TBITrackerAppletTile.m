ac6c3609644044ee9c8267f747ade59da3da9f70
Tom Kiley <tkiley@mitre.org>
2012-06-02 12:48:54 -0400

    Start of TBI Tracker

---
+++ HReader/TBITrackerAppletTile.m
@@ -0,0 +1,38 @@
+//
+//  TBITrackerAppletTile.m
+//  HReader
+//
+//  Created by Lindsay Kaye on 5/14/12.
+//  Copyright (c) 2012 MITRE Corporation. All rights reserved.
+//
+
+#import "TBITrackerAppletTile.h"
+
+@implementation TBITrackerAppletTile
+
+
+
+#pragma mark - View lifecycle
+
+- (void)tileDidLoad
+{
+    [super tileDidLoad];
+    // Do any additional setup after loading the view from its nib.
+
+}
+
+-(void)didReceiveTap:(UIViewController *)sender inRect:(CGRect)rect
+{
+    UIViewController *controller = [[UIViewController alloc] initWithNibName:@"TBITrackerDashboard" bundle: [NSBundle mainBundle]];
+    controller.title = [self.userInfo objectForKey:@"display_name"];
+    [sender.navigationController pushViewController:controller animated:YES];
+   
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+	return YES;
+}
+
+@end
---
+++ HReader/TBITrackerDashboard.h
@@ -0,0 +1,13 @@
+//
+//  TBITrackerDashboard.h
+//  HReader
+//
+//  Created by Lindsay Kaye on 5/15/12.
+//  Copyright (c) 2012 MITRE Corporation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TBITrackerDashboard : UIViewController
+
+@end
