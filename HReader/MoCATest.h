aea492b2dcced554fa20ad5e33eb60621190abc8
Lindsay Kaye <lkaye@MM170465-PC.local>
2012-07-02 16:03:03 -0400

    MoCA test stuff

--- HReader/MoCATest.h
+++ HReader/MoCATest.h
@@ -10,4 +10,13 @@
 
 @interface MoCATest : UIViewController
 
+- (IBAction) nextClicked: (id)sender;
+- (IBAction) prevClicked: (id)sender;
+
+@property (retain, nonatomic) IBOutlet UIView *displayArea;
+@property (retain, nonatomic) IBOutlet UILabel *testLabel;
+@property (retain, nonatomic) IBOutlet UIButton *nextButton;
+@property (retain, nonatomic) IBOutlet UIButton *prevButton;
+@property (retain, nonatomic) IBOutlet UIProgressView *progressBar;
+
 @end
---
+++ HReader/TBITrackerLearn.h
@@ -0,0 +1,13 @@
+//
+//  TBITrackerLearn.h
+//  HReader
+//
+//  Created by Lindsay Kaye on 6/27/12.
+//  Copyright (c) 2012 MITRE Corporation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TBITrackerLearn : UIViewController
+
+@end
