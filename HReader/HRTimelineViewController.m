3ab08fd5e927aba87701f2c3f7107a83938265e5
Caleb Davenport <cdavenport@mitre.org>
2012-09-18 13:42:56 -0400

    Code cleanup and prepare for switch to JSON.

--- HReader/HRTimelineViewController.m
+++ HReader/HRTimelineViewController.m
@@ -6,6 +6,9 @@
 //  Copyright (c) 2011 MITRE Corporation. All rights reserved.
 //
 
+#define HR_TIMELINE_XML 1
+#define HR_TIMELINE_JSON !HR_TIMELINE_XML
+
 #import <QuartzCore/QuartzCore.h>
 
 #import "HRTimelineViewController.h"
@@ -18,20 +21,7 @@
 
 #import "DDXML.h"
 
-#if !__has_feature(objc_arc)
-#error This class requires ARC
-#endif
-
-@interface HRTimelineViewController ()
-- (void)reloadData;
-@end
-
 @implementation HRTimelineViewController
-
-@synthesize webView     = _webView;
-@synthesize headerView  = _headerView;
-@synthesize nameLabel   = _nameLabel;
-@synthesize patientImageView = _patientImageView;
 
 #pragma mark - object methods
 
@@ -58,18 +48,16 @@
 - (void)reloadData {
     if ([self isViewLoaded]) {
         
-        // vars
-        NSURL *URL;
-        
         // load patient
         HRMPatient *patient = [(id)self.panelViewController.leftAccessoryViewController selectedPatient];
         self.patientImageView.image = [patient patientImage];
-        
         self.nameLabel.text = [patient.compositeName uppercaseString];
-        URL = [[[NSFileManager defaultManager]
-                URLsForDirectory:NSDocumentDirectory
-                inDomains:NSUserDomainMask]
-               lastObject];
+        
+#if HR_TIMELINE_XML
+        NSURL *URL = [[[NSFileManager defaultManager]
+                       URLsForDirectory:NSDocumentDirectory
+                       inDomains:NSUserDomainMask]
+                      lastObject];
         URL = [URL URLByAppendingPathComponent:@"hReader.xml"];
         NSString *XMLString = [[[patient timelineXMLDocument] XMLString] copy];
         NSData *XMLData = [XMLString dataUsingEncoding:NSUTF8StringEncoding];
@@ -86,6 +74,15 @@
                subdirectory:@"timeline/hReader"];
         NSURLRequest *request = [NSURLRequest requestWithURL:URL];
         [self.webView loadRequest:request];
+#endif
+#if HR_TIMELINE_JSON
+        NSError *error = nil;
+        NSDictionary *dictionary = nil;
+        NSData *data = [NSJSONSerialization dataWithJSONObject:dictionary options:0 error:&error];
+        NSString *string = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+        string = [NSString stringWithFormat:@"function(%@);", string];
+        [self.webView stringByEvaluatingJavaScriptFromString:string];
+#endif
         
     }
 }
@@ -122,6 +119,17 @@
         gesture.numberOfTouchesRequired = 1;
         [self.patientImageView.superview addGestureRecognizer:gesture];
     }
+    
+    // load html document
+#if HR_TIMELINE_JSON
+    NSURL *URL = [[NSBundle mainBundle]
+                  URLForResource:@"hReader"
+                  withExtension:@"html"
+                  subdirectory:@"timeline/hReader"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:URL];
+    [self.webView loadRequest:request];
+#endif
+    
 }
 
 - (void)viewDidUnload {
