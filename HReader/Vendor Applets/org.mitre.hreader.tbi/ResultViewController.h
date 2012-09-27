2730eba4a581c3b8aba274de745787ffb08924f4
Saltzman <ssaltzman@mm170465-mac.mitre.org>
2012-09-27 14:49:09 -0400

    UI overhaul

---
+++ HReader/Vendor Applets/org.mitre.hreader.tbi/ResultViewController.h
@@ -0,0 +1,13 @@
+//
+//  ResultViewController.h
+//  HReader
+//
+//  Created by Saltzman, Shep on 9/25/12.
+//  Copyright (c) 2012 MITRE Corporation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ResultViewController : UIViewController
+
+@end
---
+++ HReader/Vendor Applets/org.mitre.hreader.tbi/ResultViewController.m
@@ -0,0 +1,38 @@
+//
+//  ResultViewController.m
+//  HReader
+//
+//  Created by Saltzman, Shep on 9/25/12.
+//  Copyright (c) 2012 MITRE Corporation. All rights reserved.
+//
+
+#import "ResultViewController.h"
+
+@interface ResultViewController ()
+
+@end
+
+@implementation ResultViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
