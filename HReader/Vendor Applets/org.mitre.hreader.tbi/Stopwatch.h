0bab91ffcdaa8279a67d9eed45f74f0f37863341
ssaltzman <ssaltzman@mitre.org>
2012-10-02 14:36:30 -0400

    Actually fixed project.pbxproj this time. Also added the beginnings of the Stopwatch utility class.

---
+++ HReader/Vendor Applets/org.mitre.hreader.tbi/Stopwatch.h
@@ -0,0 +1,13 @@
+//
+//  Stopwatch.h
+//  HReader
+//
+//  Created by Saltzman, Shep on 10/2/12.
+//  Copyright (c) 2012 MITRE Corporation. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Stopwatch : NSObject
+
+@end
---
+++ HReader/Vendor Applets/org.mitre.hreader.tbi/Stopwatch.m
@@ -0,0 +1,13 @@
+//
+//  Stopwatch.m
+//  HReader
+//
+//  Created by Saltzman, Shep on 10/2/12.
+//  Copyright (c) 2012 MITRE Corporation. All rights reserved.
+//
+
+#import "Stopwatch.h"
+
+@implementation Stopwatch
+
+@end
