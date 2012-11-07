862f5862e74dc903c62ebe39a5398ff2e0fd49d9
Caleb Davenport <cdavenport@mitre.org>
2012-11-07 12:19:23 -0500

    Add initial architecture to support accepting post data from timeline.

---
+++ HReader/HRTimelinePOSTURLProtocol.h
@@ -0,0 +1,13 @@
+//
+//  HRTimelinePOSTURLProtocol.h
+//  HReader
+//
+//  Created by Caleb Davenport on 11/5/12.
+//  Copyright (c) 2012 MITRE Corporation. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HRTimelinePOSTURLProtocol : NSURLProtocol
+
+@end
