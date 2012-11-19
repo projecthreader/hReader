682414f9beb99226bdc5876ca2e3f68718e26e97
Adam B. Goldstein <abgoldstein@gmail.com>
2012-11-19 16:24:35 -0500

    Adding and removing initial files for adding popHealth applet.

---
+++ HReader/HRPophealthAppletTile.h
@@ -0,0 +1,13 @@
+//
+//  HRPophealthAppletTile.h
+//  HReader
+//
+//  Created by Adam Goldstein on 11/19/12.
+//  Copyright (c) 2012 MITRE Corporation. All rights reserved.
+//
+
+#import "HRWebViewAppletTile.h"
+
+@interface HRPophealthAppletTile : HRWebViewAppletTile
+
+@end
---
+++ HReader/HRPophealthAppletTile.m
@@ -0,0 +1,31 @@
+//
+//  HRPophealthAppletTile.m
+//  HReader
+//
+//  Created by Adam Goldstein on 11/19/12.
+//  Copyright (c) 2012 MITRE Corporation. All rights reserved.
+//
+
+#import "HRPophealthAppletTile.h"
+
+@implementation HRPophealthAppletTile
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
