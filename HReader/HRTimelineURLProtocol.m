cd53310b8038070446fd2e176fd6d9587ae977a3
Caleb Davenport <cdavenport@mitre.org>
2012-11-19 12:22:57 -0500

    Resolve threading issue in timeline GET protocol.

--- HReader/HRTimelineURLProtocol.m
+++ HReader/HRTimelineURLProtocol.m
@@ -46,53 +46,56 @@
 }
 
 - (void)startLoading {
-    [[HRAppDelegate managedObjectContext] performBlock:^{
-        NSURL *URL = [[self request] URL];
+    
+    // load calendar
+    static NSCalendar *calendar = nil;
+    static dispatch_once_t token;
+    dispatch_once(&token, ^{
+        calendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar];
+    });
+    
+    // determine scope
+    NSURL *URL = [[self request] URL];
+    NSDate *date = [NSDate date];
+    NSDictionary *parameters = [HRAPIClient parametersFromQueryString:[URL query]];
+    NSPredicate *predicate = nil;
+    NSDateComponents *components = [[NSDateComponents alloc] init];
+    NSString *page = [parameters objectForKey:@"page"];
+    if ([page isEqualToString:@"day"]) { [components setDay:-1]; }
+    else if ([page isEqualToString:@"week"]) { [components setWeek:-1]; }
+    else if ([page isEqualToString:@"month"]) { [components setMonth:-1]; }
+    else if ([page isEqualToString:@"year"]) { [components setYear:-1]; }
+    else if ([page isEqualToString:@"decade"]) { [components setYear:-10]; }
+    else { components = nil; }
+    if (components) {
+        date = [calendar dateByAddingComponents:components toDate:date options:0];
+        predicate = [NSPredicate predicateWithFormat:@"date >= %@", date];
+    }
+    
+    // get data
+    __block NSData *data = nil;
+    __block NSError *error = nil;
+    [[HRAppDelegate managedObjectContext] performBlockAndWait:^{
         HRMPatient *patient = [HRPeoplePickerViewController selectedPatient];
-        
-        // load calendar
-        static NSCalendar *calendar = nil;
-        static dispatch_once_t token;
-        dispatch_once(&token, ^{
-            calendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar];
-        });
-        
-        // determine scope
-        NSDate *date = [NSDate date];
-        NSDictionary *parameters = [HRAPIClient parametersFromQueryString:[URL query]];
-        NSPredicate *predicate = nil;
-        NSDateComponents *components = [[NSDateComponents alloc] init];
-        NSString *page = [parameters objectForKey:@"page"];
-        if ([page isEqualToString:@"day"]) { [components setDay:-1]; }
-        else if ([page isEqualToString:@"week"]) { [components setWeek:-1]; }
-        else if ([page isEqualToString:@"month"]) { [components setMonth:-1]; }
-        else if ([page isEqualToString:@"year"]) { [components setYear:-1]; }
-        else if ([page isEqualToString:@"decade"]) { [components setYear:-10]; }
-        else { components = nil; }
-        if (components) {
-            date = [calendar dateByAddingComponents:components toDate:date options:0];
-            predicate = [NSPredicate predicateWithFormat:@"date >= %@", date];
-        }
-        
-        // load data
-        id<NSURLProtocolClient> client = [self client];
-        NSError *error = nil;
-        NSData *data = [patient timelineJSONPayloadWithPredicate:predicate error:&error];
-        if ([data length]) {
-            NSHTTPURLResponse *response = [[NSHTTPURLResponse alloc]
-                                           initWithURL:[[self request] URL]
-                                           MIMEType:@"application/json"
-                                           expectedContentLength:[data length]
-                                           textEncodingName:nil];
-            [client URLProtocol:self didReceiveResponse:response cacheStoragePolicy:NSURLCacheStorageNotAllowed];
-            [client URLProtocol:self didLoadData:data];
-            [client URLProtocolDidFinishLoading:self];
-        }
-        else {
-            [client URLProtocol:self didFailWithError:error];
-        }
-        
+        data = [patient timelineJSONPayloadWithPredicate:predicate error:&error];
     }];
+    
+    // send response
+    id<NSURLProtocolClient> client = [self client];
+    if ([data length]) {
+        NSHTTPURLResponse *response = [[NSHTTPURLResponse alloc]
+                                       initWithURL:[[self request] URL]
+                                       MIMEType:@"application/json"
+                                       expectedContentLength:[data length]
+                                       textEncodingName:nil];
+        [client URLProtocol:self didReceiveResponse:response cacheStoragePolicy:NSURLCacheStorageNotAllowed];
+        [client URLProtocol:self didLoadData:data];
+        [client URLProtocolDidFinishLoading:self];
+    }
+    else {
+        [client URLProtocol:self didFailWithError:error];
+    }
+    
 }
 
 - (void)stopLoading {
