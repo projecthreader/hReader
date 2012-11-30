56e4613da3ecc89abf89fddd1c80c1a26da96e78
Adam B. Goldstein <abgoldstein@gmail.com>
2012-11-30 16:28:41 -0500

    Adding HTML/CSS mockup of pophealth applet.

---
+++ HReader/HRPopHealthAppletTile.h
@@ -0,0 +1,13 @@
+//
+//  HRPopHealthAppletTile.h
+//  HReader
+//
+//  Created by Adam Goldstein on 11/26/12.
+//  Copyright (c) 2012 MITRE Corporation. All rights reserved.
+//
+
+#import "HRWebViewAppletTile.h"
+
+@interface HRPopHealthAppletTile : HRWebViewAppletTile
+
+@end
