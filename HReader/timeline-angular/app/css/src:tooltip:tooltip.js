28e664874aba2456588e55ee81f63b991752d5d1
McCloud, Jeff <jmccloud@mitre.org>
2013-02-05 13:21:59 -0500

    HReader/timeline-angular/app/css/master.css

---
+++ HReader/timeline-angular/app/css/src:tooltip:tooltip.js
@@ -0,0 +1,1 @@
+src/tooltip/tooltip.js
---
+++ HReader/timeline-angular/app/js/example.js
@@ -0,0 +1,4 @@
+var TooltipDemoCtrl = function ($scope) {
+  $scope.dynamicTooltip = "I'll tell you what you just told you";
+  $scope.dynamicTooltipText = "Type something awesome";
+};
